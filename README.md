b3b24ee7a0e076e1ad7eb5d72b4821efad3222fb
Nolan Ross <nolanross917@gmail.com>
2023-04-04 13:50:30 -0400

    refactored code and added data

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # sql-employee-tracker
+
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+  ## Description
+  This is a simple employee tracker application built using SQL. The application allows you to manage employees, departments, and roles in a company.
+
+  ## Table of Contents
+
+  *[Description](#description)
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+  *[Contributing](#contributing)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+  *[Credits](credits)
+
+  ## Installation
+  Clone this repository to your local machine.
+
+  Open a terminal or command prompt window and navigate to the root directory of the project.
+
+  Run the following command to create the database schema:
+  `$ mysql -u root -p < schema.sql`
+  Start the application by running the following command:
+  `node app.js`
+  Follow the prompts to manage employees, departments, and roles.
+
+  ## License
+    This project is covered under the MIT license. To learn more about what this means, click the license badge at the top.
+
+  ## Contributing
+  If you would like to contribute to this project, please fork the repository and submit a pull request.
+
+  ## Questions
+  Have questions about this project?
+
+  Name: Nolan Ross
+
+  Github: https://github.com/beefbones
+
+  Email: nolanross917@gmail.com
+  
---
+++ server.js
@@ -0,0 +1,188 @@
+const inquirer = require('inquirer');
+const dbConnection = require ('./config/connection')
+
+function initPrompt () {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Please select what you would like to do:',
+        name: 'select',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee role',
+            'end'
+        ]
+    })
+    .then(function ({select}) {
+        switch (select) {
+            case 'view all departments':
+                viewDepartments();
+                break;
+            
+            case 'view all roles':
+                viewRoles();
+                break;
+
+            case 'view all employees':
+                viewEmployees();
+                break;
+
+            case 'add a department':
+                addDepartment();
+                break;
+            
+            case 'add a role':
+                addRole();
+                break;
+
+            case 'add an employee':
+                addEmployee();
+                break;
+
+            case 'update an employee role':
+                updateEmployee();
+                break;
+
+            case 'end':
+                connection.end();
+                break;
+        }
+    })
+}
+
+function viewDepartments () {
+    const query = `SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY DEPARTMENT');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+initPrompt();
+}
+
+function viewRoles () {
+    const query = `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;`;
+    dbConnection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY ROLE');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+initPrompt();
+}
+
+function viewEmployees () {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+    dbConnection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        prompt();
+    });
+initPrompt();
+}
+
+function addDepartment () {
+
+initPrompt();
+}
+
+function addRole () {
+
+initPrompt();
+}
+
+function addEmployee () {
+    const addname = await inquirer.prompt(askName());
+    dbConnection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employee role?: '
+            }
+        ]);
+        let roleId;
+        for (const row of res) {
+            if (row.title === role) {
+                roleId = row.id;
+                continue;
+            }
+        }
+        dbConnection.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res => `${res.first_name} ${res.last_name}`);
+            choices.push('none');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'manager',
+                    type: 'list',
+                    choices: choices,
+                    message: 'Choose the employee Manager: '
+                }
+            ]);
+            let managerId;
+            let managerName;
+            if (manager === 'none') {
+                managerId = null;
+            } else {
+                for (const data of res) {
+                    data.fullName = `${data.first_name} ${data.last_name}`;
+                    if (data.fullName === manager) {
+                        managerId = data.id;
+                        managerName = data.fullName;
+                        console.log(managerId);
+                        console.log(managerName);
+                        continue;
+                    }
+                }
+            }
+            console.log('Employee has been added. Please view all employee to verify...');
+            dbConnection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: addname.first,
+                    last_name: addname.last,
+                    role_id: roleId,
+                    manager_id: parseInt(managerId)
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    prompt();
+
+                }
+            );
+        });
+    });
+initPrompt();
+}
+
+function updateEmployee () {
+
+initPrompt();
+}
+
+initPrompt();
